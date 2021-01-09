42e5a1bdf4cd8fa272d52ec9199c7ed5d2fdfc7a
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2021-01-08 22:07:40 -0600

    Tablas corregidas a partir de nuevo diagrama entidad relación
    Ya estan las tablas basadas en el diagrama ER corregido, solo falta ponerle a los atributos su tipo de datos correcto

--- Libreria.sql
+++ Libreria.sql
@@ -6,135 +6,103 @@ create database libreria;
 use libreria;
 
 /*===============tablas============================================================*/
+create table cliente(
+  idCliente int not null primary key,
+  nombres int not null,
+  paterno int not null,
+  materno int not null,
+  correo int not null,
+  contrasena int not null
+);
+
 create table autor(
-  idAutor int not null,
-  nombres varchar(50) not null,
-  paterno varchar(50) not null,
-  materno varchar(50) not null,
-  primary key (idAutor)
+  idAutor int not null primary key,
+  nombres int not null,
+  paterno int not null,
+  materno int not null
 );
 
 create table editorial(
-  idEditorial int not null,
-  nombre varchar(50) not null,
+  idEditorial int not null primary key,
+  nombre int not null,
   telefono int not null,
-  sitioweb varchar(50) not null,
-  primary key (idEditorial)
-);
-
-create table paginas(
-  idPaginas int not null,
-  numeroPaginas int not null,
-  primary key (idPaginas)
-);
-
-create table correo(
-  idCorreo int not null,
-  correo varchar(50) not null,
-  primary key (idCorreo)
+  sitioweb int not null
 );
 
 create table idioma(
-  idIdioma int not null,
-  idioma varchar(20) not null,
-  primary key (idIdioma)
+  idIdioma int not null primary key,
+  idioma int not null
 );
 
-create table cliente(
-  idCliente int not null,
-  nombres varchar(50) not null,
-  paterno varchar(50)  not null,
-  materno varchar(50)  not null,
-  idCorreo int not null,
-  primary key (idCliente),
-  foreign key (idCorreo) references correo(idCorreo)
+create table formato(
+  idFormato int not null primary key,
+  tipoformato int not null
 );
 
 create table datos(
-  idDatos int not null,
-  nombre varchar(50) not null,
-  anio int not null,
-  portada varchar(50) not null,
+  idDatos int not null primary key,
   idIdioma int not null,
-  primary key (idDatos),
+  nombre int not null,
+  anio int not null,
+  portada int not null,
   foreign key (idIdioma) references idioma(idIdioma)
-);
-
-create table contrasena(
-  idContraseña int not null,
-  idCliente int not null,
-  contrasena nvarchar(50) not null,
-  primary key (idContraseña),
-  foreign key (idCliente) references cliente(idCliente)
 );
 
 create table edita(
   idDatos int not null,
   idEditorial int not null,
+  numeroPaginas int not null,
   primary key (idDatos, idEditorial),
   foreign key (idDatos) references datos(idDatos),
   foreign key (idEditorial) references editorial(idEditorial)
 );
 
 create table libro(
-  sku int not null,
+  sku int not null primary key,
+  idDatos int not null,
   edicion int not null,
   disponibilidad int not null,
-  idDatos int not null,
-  idPaginas int not null,
-  primary key (sku),
-  foreign key (idDatos) references datos(idDatos),
-  foreign key (idPaginas) references paginas(idPaginas)
+  foreign key (idDatos) references datos(idDatos)
 );
 
 create table audiolibro(
-  sku int not null,
-  duracion int not null,
-  narradorNombres varchar(50) not null,
-  narradorPaterno varchar(50) not null,
-  narradorMaterno varchar(50) not null,
+  sku int not null primary key,
   idDatos int not null,
-  primary key (sku),
+  duracion int not null,
+  narradornombres int not null,
+  narradorpaterno int not null,
+  narradormaterno int not null,  
   foreign key (idDatos) references datos(idDatos)
 );
 
 create table ebook(
-  sku int not null,
-  anioDigitalizacion int not null,
-  formato varchar(50) not null,
+  sku int not null primary key,
   idDatos int not null,
-  idPaginas int not null,
-  primary key (sku),
+  skuLibro int not null,
+  idFormato int not null,
+  anioDigitalizacion int not null,
   foreign key (idDatos) references datos(idDatos),
-  foreign key (idPaginas) references paginas(idPaginas)
+  foreign key (skuLibro) references libro(sku),
+  foreign key (idFormato) references formato(idFormato)
 );
 
-create table escribeLibro(
-  sku int not null,
+create table escribe(
   idAutor int not null,
+  sku int not null,
   primary key (idAutor, sku),
   foreign key (idAutor) references autor(idAutor),
   foreign key (sku) references libro(sku)
 );
 
-create table escribeAudiolibro(
-  sku int not null,
+create table redacta(
   idAutor int not null,
+  sku int not null,
   primary key (sku, idAutor),
   foreign key (sku) references audiolibro(sku),
   foreign key (idAutor) references autor(idAutor)
 );
 
-create table escribeEbook(
-  sku int not null,
-  idAutor int not null,
-  primary key (sku, idAutor),
-  foreign key (sku) references ebook(sku),
-  foreign key (idAutor) references autor(idAutor)
-);
-
-#hmmm quiza esta tabla no la usemos
-create table consultaLibro(
+create table consulta(
   idCliente int not null,
   sku int not null,
   primary key (idCliente, sku),
